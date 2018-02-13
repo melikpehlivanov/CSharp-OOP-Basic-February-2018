af6464dd7924c248a8c19c798d52d5ae31e0fabd
melik <melikpehlivanov@gmail.com>
2018-02-13 15:47:59 +0200

    If you're going to submit this in judge.softuni.bg, please use the .net core strategy or you will get time limit exception.

---
+++ Defining Classes - Exercise/02. Creating Constructors/Person.cs
@@ -0,0 +1,37 @@
+﻿public class Person
+{
+    private string name;
+    private int age;
+
+    public Person()
+    {
+        this.Name = "No name";
+        this.Age = 1;
+    }
+
+    public Person(int age)
+        : this()
+    {
+        this.Age = age;
+    }
+
+    public Person(string name, int age)
+        : this(age)
+    {
+        this.Name = name;
+        this.Age = age;
+    }
+
+    public string Name
+    {
+        get => this.name;
+        set => this.name = value;
+    }
+
+    public int Age
+    {
+        get => this.age;
+        set => this.age = value;
+    }
+}
+
---
+++ Defining Classes - Exercise/02. Creating Constructors/StartUp.cs
@@ -0,0 +1,9 @@
+﻿namespace _02.Creating_Constructors
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+        }
+    }
+}
