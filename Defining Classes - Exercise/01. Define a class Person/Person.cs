b54f7e7e12f0cca77ebe523d4fd843d18f96bc5f
melik <melikpehlivanov@gmail.com>
2018-02-13 15:47:21 +0200

    If you're going to submit this in judge.softuni.bg, please use the .net core strategy.

---
+++ Defining Classes - Exercise/01. Define a class Person/Person.cs
@@ -0,0 +1,6 @@
+﻿public class Person
+{
+    public string Name { get; set; }
+
+    public int Age { get; set; }
+}
---
+++ Defining Classes - Exercise/01. Define a class Person/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Reflection;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        Type personType = typeof(Person);
+        PropertyInfo[] properties = personType.GetProperties
+            (BindingFlags.Public | BindingFlags.Instance);
+        Console.WriteLine(properties.Length);
+    }
+}
+
