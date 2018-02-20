0e9881c9abe9c6db01e3a4567b3df39aa4fcf903
melik <melikpehlivanov@gmail.com>
2018-02-20 14:23:51 +0200

    Update.

--- Encapsulation - Exercise/02. Class Box Data Validation/StartUp.cs
+++ Encapsulation - Exercise/02. Class Box Data Validation/StartUp.cs
@@ -1,17 +1,11 @@
 ﻿namespace _02.Class_Box_Data_Validation
 {
     using System;
-    using System.Linq;
-    using System.Reflection;
 
     public class StartUp
     {
         public static void Main()
         {
-            Type boxType = typeof(Box);
-            FieldInfo[] fields = boxType.GetFields(BindingFlags.NonPublic | BindingFlags.Instance);
-            Console.WriteLine(fields.Count());
-
             try
             {
                 double lenght = double.Parse(Console.ReadLine());
