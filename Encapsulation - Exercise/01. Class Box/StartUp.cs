879bbad4a3009ab7124be4e1eb2c2c7856b5749a
melik <melikpehlivanov@gmail.com>
2018-02-20 14:23:34 +0200

    Updated StartUp

--- Encapsulation - Exercise/01. Class Box/StartUp.cs
+++ Encapsulation - Exercise/01. Class Box/StartUp.cs
@@ -1,17 +1,11 @@
 ﻿namespace _01.Class_Box
 {
     using System;
-    using System.Linq;
-    using System.Reflection;
 
     public class StartUp
     {
         public static void Main()
         {
-            Type boxType = typeof(Box);
-            FieldInfo[] fields = boxType.GetFields(BindingFlags.NonPublic | BindingFlags.Instance);
-            Console.WriteLine(fields.Count());
-
             double lenght = double.Parse(Console.ReadLine());
             double width = double.Parse(Console.ReadLine());
             double height = double.Parse(Console.ReadLine());
